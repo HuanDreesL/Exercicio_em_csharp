919da3eff6acd7a917749d45ed5cbf73ef6947ac
Huandres Schmidt <163208912+HuanDreesL@users.noreply.github.com>
2024-10-10 21:32:22 -0300

    Json

---
+++ Json/Json/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿namespace Json
+{
+    public class Pessoa
+    {
+        public string Nome { get; set;}
+        public int Idade { get; set;}
+        public string Cidade { get; set;}
+    }
+}
---
+++ Json/Json/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace Json
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa
+            {
+                Nome = "João",
+                Idade = 30,
+                Cidade = "Chapecó"
+            };
+
+            string json = JsonSerializer.Serialize(pessoa);
+            Console.WriteLine("Objeto Json: " + json);
+        }
+    }
+}
