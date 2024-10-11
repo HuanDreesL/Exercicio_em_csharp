f4be1f8ba03c454fc6ca62e0b7579fbabd34012a
Huandres Schmidt <163208912+HuanDreesL@users.noreply.github.com>
2024-10-10 21:31:48 -0300

    ler arquivo XML

---
+++ Ler arquivo XML e serializar/Ler arquivo XML e serializar/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿namespace Ler_arquivo_XML_e_serializar
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Cidade { get; set; }
+    }
+}
---
+++ Ler arquivo XML e serializar/Ler arquivo XML e serializar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization;
+
+namespace Ler_arquivo_XML_e_serializar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader xml = new StreamReader(@"C:\tmp\pessoa.xml")) 
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Pessoa));
+                Pessoa pessoa = (Pessoa)serializer.Deserialize(xml);
+
+                Console.WriteLine($"Nome: {pessoa.Nome}");
+                Console.WriteLine($"Idade: {pessoa.Idade}");
+                Console.WriteLine($"Cidade: {pessoa.Cidade}");
+            }
+
+        }
+    }
+}
